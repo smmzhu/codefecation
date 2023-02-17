03c036a6bffec0299e702fef5004a7d6c4770c2d
Christopher Lai <christopherlai@ucsb.edu>
2023-02-17 00:57:04 -0800

    deleting useless files

--- project/components/Map.jsx
+++ project/components/Map.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
-import './map.css'
+//import './map.css'
+
+//import 'key.env'
+// import { Icon } from '@iconify/react'
+// import locationIcon from '@iconify/icons-mdi/map-marker'
 
 const location = {
     address: 'Lagoon Rd, Isla Vista, California',
     lat: 34.404834,
     lng: -119.844177,
-  }
+}
 
 const Map = ({ location, zoomLevel }) => (
     <div className="map">
@@ -14,16 +18,25 @@ const Map = ({ location, zoomLevel }) => (
   
     <div className="google-map">
         <GoogleMapReact
-            bootstrapURLKeys={{ key: '' }}
+            bootstrapURLKeys={{ key: (key.env.REACT_APP_GOOGLE_MAPS_API_KEY) }}
             defaultCenter={location}
             defaultZoom={zoomLevel}
         >
-                <LocationPin
-                    lat={location.lat}
-                    lng={location.lng}
-                    text={location.address}
-                />
-            </GoogleMapReact>
+            <LocationPin
+                lat={location.lat}
+                lng={location.lng}
+                text={location.address}
+            />
+        </GoogleMapReact>
         </div>
     </div>
 )
+
+// const LocationPin = ({ text }) => (
+//     <div className="pin">
+//       <Icon icon={locationIcon} className="pin-icon" />
+//       <p className="pin-text">{text}</p>
+//     </div>
+// )
+
+export default Map
